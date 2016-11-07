9f3392669f6f4c3cb36f52dcea5a265532c58976
e_wait@126.com <e_wait@126.com>
2016-11-07 17:52:36 +0800

    layui nav

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  res.render('index', { title: 'Express', host : req.host, showHead : true });
 });
 
 module.exports = router;
